c01b4d5ee0b5799c71a0c3e92280af30f9bb8fa7
davidbuzatu-marian <davidm.buzatu@gmail.com>
2019-11-04 21:11:40 +0200

    Updates

---
+++ precache-manifest.2c37a759a3dddc6e6e30dabdda09b93c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b20d4a125a080ea56ba6493853cacb",
+    "url": "/SortingMethodsWebsite/index.html"
+  },
+  {
+    "revision": "e905aa59bd5056baf131",
+    "url": "/SortingMethodsWebsite/static/css/main.9582b734.chunk.css"
+  },
+  {
+    "revision": "1151e15d50ff42707340",
+    "url": "/SortingMethodsWebsite/static/js/2.e01df164.chunk.js"
+  },
+  {
+    "revision": "e905aa59bd5056baf131",
+    "url": "/SortingMethodsWebsite/static/js/main.08c166ad.chunk.js"
+  },
+  {
+    "revision": "8f42f701dd496ddeac71",
+    "url": "/SortingMethodsWebsite/static/js/runtime-main.ac7ea5d6.js"
+  }
+]);
