016b5b37e728cc8a77ca711586e9abc9f3e1d6b2
davidbuzatu-marian <davidm.buzatu@gmail.com>
2019-11-06 21:16:12 +0200

    Updates

---
+++ precache-manifest.39d97621bc4ea8baf67a2b4999c92a84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e68a189b983f4647de1948dc69b626",
+    "url": "/SortingMethodsWebsite/index.html"
+  },
+  {
+    "revision": "909c7c027d9664892f40",
+    "url": "/SortingMethodsWebsite/static/css/main.8446569b.chunk.css"
+  },
+  {
+    "revision": "379a22e0852e58d63d04",
+    "url": "/SortingMethodsWebsite/static/js/2.e0f6182b.chunk.js"
+  },
+  {
+    "revision": "909c7c027d9664892f40",
+    "url": "/SortingMethodsWebsite/static/js/main.4719981a.chunk.js"
+  },
+  {
+    "revision": "8f42f701dd496ddeac71",
+    "url": "/SortingMethodsWebsite/static/js/runtime-main.ac7ea5d6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/VisualizeSortingMethods/precache-manifest.c32711cdff3feef6f07e0cc91aa00fa5.js"
+  "/SortingMethodsWebsite/precache-manifest.39d97621bc4ea8baf67a2b4999c92a84.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/VisualizeSortingMethods/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SortingMethodsWebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
